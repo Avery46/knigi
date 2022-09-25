eef7a3b4d85332f090cdc50452352ac4e912225f
Avery46 <kursakovegor46@gmail.com>
2022-09-25 14:13:29 +0300

    ind

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
-<?php include_once("home.html");include_once("katalog.html");include_once("katalog1.html");
+<?php include_once("index.html");include_once("katalog.html");include_once("katalog1.html");
 include_once("ff.html");include_once("kont.html");include_once("new.html");include_once("oplata.html"); ?>
